1d467cfc0505a32e2ad0930692ce2756fe55168e
acms26 <127745045+acms26@users.noreply.github.com>
2023-03-26 14:01:24 +0200

    Update scraping.sh

--- scraping.sh
+++ scraping.sh
@@ -5,6 +5,6 @@ html=$(curl -s "$url")
 datetime=$(date +"%Y-%m-%d %H:%M:%S")
 
 world_population=$(echo "$html" | grep -Po '(?<=<td class="counter"><div id="cp1">)[^<]+')
-population_growth_today=$(echo "$html" | grep -Po '(?<=<td class="counter"><div id="cp13">)[^<]+')
+population_growth_year_to_date=$(echo "$html" | grep -Po '(?<=<td class="counter"><div id="cp12">)[^<]+')
 
-echo "$datetime;$world_population;$population_growth_today" >> /home/arnaudchn/PROJET/data.csv
+echo "$datetime;$world_population;$population_growth_year_to_date" >> /home/arnaudchn/PROJET/data.csv
