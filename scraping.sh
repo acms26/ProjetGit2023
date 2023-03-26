a93e9184f388f751aeb9cab98c23dc8a78a08935
arnaudchn <acms26.pro@gmail.com>
2023-03-26 11:13:55 +0200

    bash modification

--- scraping.sh
+++ scraping.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-curl -s https://flightaware.com/live/airport/LFPO | grep -oP '<span class="flightRow.*?">\K.*?(?=</span>)' | awk '{printf "%s%s\n", $0, (NR%6==0 ? "\n" : "\t")}'
 
+url="https://countrymeters.info/en/World"
+html=$(curl -s "$url")
+datetime=$(date +"%Y-%m-%d %H:%M:%S")
+
+population_growth_today=$(echo "$html" | grep -Po '(?<=<td class="counter"><div id="cp13">)[^<]+')
+
+echo "$population_growth_today" >> /home/arnaudchn/PROJET/data.csv
