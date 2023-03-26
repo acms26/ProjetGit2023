108a0d0be818cf5a35ed6112082c96bb4063b451
acms26 <127745045+acms26@users.noreply.github.com>
2023-03-26 12:02:20 +0200

    Update scraping.sh

--- scraping.sh
+++ scraping.sh
@@ -4,6 +4,7 @@ url="https://countrymeters.info/en/World"
 html=$(curl -s "$url")
 datetime=$(date +"%Y-%m-%d %H:%M:%S")
 
+world_population=$(echo "$html" | grep -Po '(?<=<td class="counter"><div id="cp1">)[^<]+')
 population_growth_today=$(echo "$html" | grep -Po '(?<=<td class="counter"><div id="cp13">)[^<]+')
 
-echo "$population_growth_today" >> /home/arnaudchn/PROJET/data.csv
+echo "$datetime;$world_population;$population_growth_today" >> /home/arnaudchn/PROJET/data.csv
